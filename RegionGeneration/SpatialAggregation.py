1ad66204aef87befd275647d372c42e5a4fcbc45
guil <guil21@mails.tsinghua.edu.cn>
2024-02-23 20:08:37 +0800

    refactor: remove dependency on pandas

--- RegionGeneration/SpatialAggregation.py
+++ RegionGeneration/SpatialAggregation.py
@@ -1,17 +1,18 @@
+from collections import defaultdict
 from copy import deepcopy
 from datetime import datetime
-from typing import Dict, List
+from typing import Callable, Dict, List
 
-import pandas as pd
+import numpy as np
 from shapely.geometry import Polygon, Point
-import pandas as pd
 
 
 def spatial_aggregation(
         uctb_data: Dict, 
         regions: List[Polygon], 
         regions_build_time: List[str] = None,
-        regions_name: List[str] = None
+        regions_name: List[str] = None,
+        agg_func: Callable = np.sum
     ):
     """Aggregates the data from the UCTB data to the regions specified in the regions dictionary.
 
@@ -24,58 +25,50 @@ def spatial_aggregation(
         regions: list with each element as shapely.geometry.Polygon
         regions_build_time: list of strings, the date format should be '%m/%d/%Y %H:%M'
         regions_name: list of strings, the name of the regions
+        agg_func: the function to aggregate the traffic data, default is np.sum
     
     Returns:
         new_uctb_data: UCTBData object after spatial aggregation
     """
     station_info = uctb_data['Node']['StationInfo']
-    if isinstance(station_info[4], list):
-        # Bike_Chicago: [..., [name]], instead of [..., name]
-        station_info = [[s[0], s[1], s[2], s[3], s[4][0]] for s in station_info]
     node_traffic = uctb_data['Node']['TrafficNode']
 
+    # element as [id, [build-time], lat, lng, name]
     new_station_info = [[None, [], r.centroid.y, r.centroid.x, ''] for r in regions]
     station_map = {}
 
-    for station in station_info:
-        for i, region in enumerate(regions):
+    for s_idx, station in enumerate(station_info):
+        for r_idx, region in enumerate(regions):
             if region.contains(Point(station[3], station[2])):
                 # id
-                station_map[station[0]] = 0
-                # build-time
+                new_station_info[r_idx][0] = -1  
+                # build_time
                 if regions_build_time is None:
-                    new_station_info[i][1].append(station[1])
+                    new_station_info[r_idx][1].append(station[1])  
                 else:
-                    new_station_info[i][1] = regions_build_time[i]
+                    new_station_info[r_idx][1] = regions_build_time[r_idx]
                 # name
                 if regions_name is not None:
-                    new_station_info[i][4] = regions_name[i]
+                    new_station_info[r_idx][4] = regions_name[r_idx]
 
-                station_map[station[4]] = i
+                station_map[s_idx] = r_idx
                 break
 
     # some regions may not include any station
     new_station_info = [s for s in new_station_info if s[0] is not None]
 
     # some stations may not be included in any region
-    i = len(regions)
-    for station in station_info:
-        name = station[4]
-        if station_map.get(name) is None:
-            station_map[name] = i
+    r_idx = len(regions)
+    for s_idx, station in enumerate(station_info):
+        if station_map.get(s_idx) is None:
+            station_map[s_idx] = r_idx
             new_station_info.append(station)
-            i += 1
+            r_idx += 1
 
     # aggregate the traffic data
-    df = pd.DataFrame(node_traffic, columns=[s[4] for s in station_info])
-    df = df.T
+    new_node_traffic = aggregate_node_traffic(node_traffic, station_map, agg_func)
 
-    df['region'] = df.index.map(station_map)
-    df = df.groupby('region').sum()
-    df = df.T
-    new_node_traffic = df.values
-
-    # reset the id of the stations
+    # reset the id of the stations to 1, 2, 3, ...
     for i, station in enumerate(new_station_info):
         station[0] = i + 1
         if regions_name is None:
@@ -96,3 +89,23 @@ def spatial_aggregation(
     new_uctb_data['Node']['StationInfo'] = new_station_info
     
     return new_uctb_data
+
+
+def aggregate_node_traffic(
+    node_traffic: np.array, 
+    station_map: Dict[int, int],
+    func: Callable = np.sum
+    ) -> np.array:
+    node_traffic = node_traffic.T  # convert to shape of (num-of-node, time_slots)
+    
+    clusters = defaultdict(list)
+    for s_idx, r_idx in station_map.items():
+        clusters[r_idx].append(node_traffic[s_idx])
+
+    r_idx_max = max(station_map.values())
+    new_node_traffic = [func(clusters[i], axis=0) for i in range(r_idx_max + 1) if clusters[i] != []]
+    new_node_traffic = np.array(new_node_traffic)
+
+    return new_node_traffic.T
+
+
