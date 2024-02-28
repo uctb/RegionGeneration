e8949a6756a37e2c9e9589678d0448d41b1a51d8
guil <guil21@mails.tsinghua.edu.cn>
2024-02-28 15:07:27 +0800

    feat: specify handlings to process isolated stations

--- RegionGeneration/SpatialAggregation.py
+++ RegionGeneration/SpatialAggregation.py
@@ -1,10 +1,17 @@
 from collections import defaultdict
 from copy import deepcopy
 from datetime import datetime
+from enum import Enum
 from typing import Callable, Dict, List
 
 import numpy as np
 from shapely.geometry import Polygon, Point
+
+
+class Handling(Enum):
+    IGNORE = 0
+    KEEP = 1
+    AGG = 2
 
 
 def spatial_aggregation(
@@ -12,7 +19,8 @@ def spatial_aggregation(
         regions: List[Polygon], 
         regions_build_time: List[str] = None,
         regions_name: List[str] = None,
-        agg_func: Callable = np.sum
+        agg_func: Callable = np.sum,
+        handling: Handling = Handling.IGNORE
     ):
     """Aggregates the data from the UCTB data to the regions specified in the regions dictionary.
 
@@ -26,6 +34,7 @@ def spatial_aggregation(
         regions_build_time: list of strings, the date format should be '%m/%d/%Y %H:%M'
         regions_name: list of strings, the name of the regions
         agg_func: the function to aggregate the traffic data, default is np.sum
+        handling: the way to handle the stations which are not included in any region
     
     Returns:
         new_uctb_data: UCTBData object after spatial aggregation
@@ -57,13 +66,30 @@ def spatial_aggregation(
     # some regions may not include any station
     new_station_info = [s for s in new_station_info if s[0] is not None]
 
-    # some stations may not be included in any region
+    # while some stations may not be included in any region
     r_idx = len(regions)
-    for s_idx, station in enumerate(station_info):
-        if station_map.get(s_idx) is None:
-            station_map[s_idx] = r_idx
-            new_station_info.append(station)
-            r_idx += 1
+    if handling == Handling.IGNORE:
+        pass
+    elif handling == Handling.KEEP:
+        for s_idx, station in enumerate(station_info):
+            if station_map.get(s_idx) is None:
+                station_map[s_idx] = r_idx
+                new_station_info.append(station)
+                r_idx += 1
+    elif handling == Handling.AGG:
+        single_region_info = [None, [], None, None, '']
+        points = []
+        for s_idx, station in enumerate(station_info):
+            if station_map.get(s_idx) is None:
+                station_map[s_idx] = r_idx
+                single_region_info[1].append(station[1])
+                points.append([station[3], station[2]])
+        polygon = Polygon(points)
+        single_region_info[2] = polygon.centroid.y
+        single_region_info[3] = polygon.centroid.x
+        new_station_info.append(single_region_info)
+    else:
+        raise ValueError('The handling method is not supported.')
 
     # aggregate the traffic data
     new_node_traffic = aggregate_node_traffic(node_traffic, station_map, agg_func)
@@ -80,7 +106,10 @@ def spatial_aggregation(
         for station in new_station_info:
             if not isinstance(station[1], list):
                 continue
-            all_build_time = [datetime.strptime(t, date_format).timestamp() for t in station[1]]
+            all_build_time = [
+                datetime.strptime(t, date_format).timestamp() 
+                for t in station[1]
+            ]
             station[1] = datetime.fromtimestamp(max(all_build_time)).strftime(date_format)
 
 
@@ -96,16 +125,21 @@ def aggregate_node_traffic(
     station_map: Dict[int, int],
     func: Callable = np.sum
     ) -> np.array:
-    node_traffic = node_traffic.T  # convert to shape of (num-of-node, time_slots)
+    """Aggregate the traffic data of the stations to the regions."""
+
+    # convert to shape of (num-of-node, time_slots)
+    node_traffic = node_traffic.T  
     
     clusters = defaultdict(list)
     for s_idx, r_idx in station_map.items():
         clusters[r_idx].append(node_traffic[s_idx])
 
     r_idx_max = max(station_map.values())
-    new_node_traffic = [func(clusters[i], axis=0) for i in range(r_idx_max + 1) if clusters[i] != []]
+    new_node_traffic = [
+        func(clusters[i], axis=0) 
+        for i in range(r_idx_max + 1) 
+        if clusters[i] != []
+    ]
     new_node_traffic = np.array(new_node_traffic)
 
     return new_node_traffic.T
-
-
